97dd72c5a2d20852baf4abf9f4016dbf930b04ca
hemulat <hemulat@davidson.edu>
2016-07-12 14:28:03 -0400

    DB With Profs Table

--- make_new_database.py
+++ make_new_database.py
@@ -84,6 +84,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
+	
 class Course(db.Model):
     __tablename__='courses'
     id = db.Column(db.Integer, primary_key=True)
@@ -104,9 +105,9 @@ class Course(db.Model):
     class_time = db.Column(db.String(20))
     bldg_code = db.Column(db.String(10))
     room_code = db.Column(db.String(10))
-    instructor1 = db.Column(db.String(30))
-    instructor2 = db.Column(db.String(30))
-    instructor3 = db.Column(db.String(30))
+    instructor1 = db.Column(db.String(30),db.ForeignKey('professor.dbname'))
+    instructor2 = db.Column(db.String(30),db.ForeignKey('professor.dbname'))
+    instructor3 = db.Column(db.String(30),db.ForeignKey('professor.dbname'))
     course_notes = db.Column(db.String(20))
     course_attrib = db.Column(db.String(20))
     max_enroll = db.Column(db.String(10))
@@ -167,7 +168,54 @@ class Course(db.Model):
 	                 visitable+privacy+learning_outcomes+lo_status)
 
     
-	
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    email = db.Column(db.String(30))
+    fullname = db.Column(db.String(30))
+    dbname = db.Column(db.String(30))
+    
+    primary_classes = db.relationship("Course", foreign_keys = [Course.instructor1])
+    secondary_classes = db.relationship("Course", foreign_keys = [Course.instructor2])
+    tertiary_classes = db.relationship("Course", foreign_keys = [Course.instructor3])
+    
+    def __init__(self,dbname,full_name,username):
+	self.dbname=dbname
+	self.fullname = full_name
+	self.email = username	
+
+# Constant values that depend on the CSV file
+FACULTY_NAME = 1
+EMAIL = 4
+PROF_CSV ="FacultyInformation.csv"
+
+# a session object that will be used to add data
+
+def make_faculty_fromCSV(csvFile):
+
+    faculty_list = []
+
+    # open file and create reader
+    with open(csvFile, 'rU') as c:
+	reader = csv.reader(c, delimiter=',', quotechar='"',
+                            skipinitialspace=True)
+
+	row_count = 0
+	for row in reader:
+	    if row_count != 0:
+
+		f_name = row[FACULTY_NAME].strip()
+		db_name = (f_name.split(",")[0].strip() + " " + 
+		           f_name.split(",")[1].strip()[:1])
+		f_email = row[EMAIL].strip()
+
+		a_faculty = Professor(db_name,f_name,f_email)
+		
+		faculty_list.append(a_faculty)
+
+	    row_count+=1
+	    
+    return faculty_list
 	
 def build_db(courses):
     db.create_all()
@@ -186,8 +234,11 @@ def build_db(courses):
 	
 	db.session.add(course)
 	
-	
+    
+    faculty_list = make_faculty_fromCSV(PROF_CSV)
+    db.session.add_all(faculty_list)
     db.session.commit()
+    
     print "CSV COUNT = " + str(count)
     q = Course.query.count()
     print "DATABASE COUNT = ", q 
@@ -204,10 +255,11 @@ def index():
 def main():
     filename = "static/1617ClassSchedule.csv"
     courses = read_csv(filename)
-    ##build_db(courses)
+    #build_db(courses)
     
     logging.warning("MAIN")
     app.run(debug=True)
 
 if __name__ == "__main__":
     main()
+    
